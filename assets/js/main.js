6d9d512eed122cb6e8e5edb51a5c6b286d93183c
Naomi Villanueva <naovillaj@gmail.com>
2017-04-11 12:54:04 -0500

    modal2

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+var imagenClick = document.getElementById("modal");
+var imagenes = document.getElementsByClassName("modalClick");
+var boton = document.getElementById("cerrar");
+var img = document.getElementById("imagenWork");
+var body = document.getElementById("body");
+
+imagenClick.style.display = "none";
+
+for(var i = 1; i<imagenes.length; i++){
+	imagenes[i-1].addEventListener("click", function(e){
+		e.preventDefault();
+		imagenClick.style.display = "block";
+		body.setAttribute("style", "overflow:hidden");
+		img.setAttribute("src", this.src);
+	})
+}
+
+boton.addEventListener("click", function(){
+	imagenClick.style.display = "none";
+})
