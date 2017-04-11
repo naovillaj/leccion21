de60c7fdfc8d507c93b852c9120b1098a6a19444
Naomi Villanueva <naovillaj@gmail.com>
2017-04-11 13:34:39 -0500

    transition

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,19 @@ var boton = document.getElementById("cerrar");
 var img = document.getElementById("imagenWork");
 var body = document.getElementById("body");
 
-imagenClick.style.display = "none";
+//imagenClick.style.display = "none";
 
 for(var i = 1; i<imagenes.length; i++){
 	imagenes[i-1].addEventListener("click", function(e){
 		e.preventDefault();
-		imagenClick.style.display = "block";
+//		imagenClick.style.display = "block";
+		imagenClick.classList.toggle("active");
 		body.setAttribute("style", "overflow:hidden");
 		img.setAttribute("src", this.src);
 	})
 }
 
 boton.addEventListener("click", function(){
-	imagenClick.style.display = "none";
+/*	imagenClick.style.display = "none";
+*/	imagenClick.classList.toggle("active");
 })
