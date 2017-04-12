9f301fb431a5dfef5a619630688ad2d358fd371c
Naomi Villanueva <naovillaj@gmail.com>
2017-04-12 10:34:18 -0500

    borrar comentarios

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,9 @@ var boton = document.getElementById("cerrar");
 var img = document.getElementById("imagenWork");
 var body = document.getElementById("body");
 
-//imagenClick.style.display = "none";
-
 for(var i = 1; i<imagenes.length; i++){
 	imagenes[i-1].addEventListener("click", function(e){
 		e.preventDefault();
-//		imagenClick.style.display = "block";
 		imagenClick.classList.toggle("active");
 		body.setAttribute("style", "overflow:hidden");
 		img.setAttribute("src", this.src);
@@ -17,7 +14,6 @@ for(var i = 1; i<imagenes.length; i++){
 }
 
 boton.addEventListener("click", function(){
-/*	imagenClick.style.display = "none";
-*/	imagenClick.classList.toggle("active");
+	imagenClick.classList.toggle("active");
 	body.setAttribute("style", "overflow:scroll");
 })
